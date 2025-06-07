5425aa09c3c2f0d18e055b234404aaeeb96778c2
diesanchz <00090725@uca.edu.sv>
2025-06-06 21:48:50 -0600

    00090725

--- ejercicioDowhile.cpp
+++ ejercicioDowhile.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
 using namespace std;
-int main (){
-  int n, acum=0;
-cout<<"digite hasta que numero quiere llegar.\n";
-cin>>n;
 
+int main() {
+    int n;
+    cout << "¿Cuantos terminos de la serie Fibonacci deseas?: ";
+    cin >> n;
 
+    int a = 0, b = 1;
+    int contador = 0;
+
+    while (contador < n) {
+        cout << a << " ";
+        int siguiente = a + b;
+        a = b;
+        b = siguiente;
+        contador++;
+    }
+
+    cout << endl;
     return 0;
 }
